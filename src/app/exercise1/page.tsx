9cd0dcb0fc5ff736632e6b01b9a02e5f405346bd
GemaBarrera <gbarrerafront@gmail.com>
2024-12-13 10:37:35 +0100

    Create Range component

--- src/app/exercise1/page.tsx
+++ src/app/exercise1/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import Range from "../components/Range";
+
 export default function Exercise1() {
-  return <div>page1</div>;
+  const handleRangeChange = (minValue, maxValue) => {
+    console.log(`Min: ${minValue}, Max: ${maxValue}`);
+  };
+  return <Range min={20} max={200} onChange={handleRangeChange} />;
 }
